63fe1cac48dec730a9746f3e09f3fd4cd864bbed
itasouza <itasouza@yahoo.com.br>
2020-01-06 02:28:47 -0200

    Projeto usando FullCalendar + asp.net core

---
+++ ProjetoCalendario/src/ProjetoWebCalendario/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoWebCalendario.Models;
+
+namespace ProjetoWebCalendario.Controllers
+{
+    public class HomeController : Controller
+    {
+        //http://www.macoratti.net/17/06/c_listas1.htm
+        static List<Evento> eventos;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult DadosEvento()
+        {
+
+            eventos = new List<Evento>();
+            eventos.Add(new Evento(1, "Tutorial PagSeguro", "#FFD700", "", Convert.ToDateTime("2020-01-06 13:00:00"), Convert.ToDateTime("2020-01-06 14:00:00")));
+            eventos.Add(new Evento(2, "Tutorial FullCalendar editar detalhes do evento", "#0071c5", "https://www.google.com/", Convert.ToDateTime("2020-01-06 11:00:00"), Convert.ToDateTime("2020-01-06 12:00:00")));
+            eventos.Add(new Evento(3, "Tutorial FullCalendar Visualizar detalhes do evento", "#42d29d", "https://www.google.com/",  Convert.ToDateTime("2020-01-06 08:00:00"), Convert.ToDateTime("2020-01-06 09:00:00")));
+            eventos.Add(new Evento(2, "Tutorial FullCalendar editar detalhes do evento", "#0071c5", "https://www.google.com/", Convert.ToDateTime("2020-01-06 10:00:00"), Convert.ToDateTime("2020-01-06 11:00:00")));
+            eventos.Add(new Evento(3, "Tutorial FullCalendar Visualizar detalhes do evento", "#42d29d", "https://www.google.com/", Convert.ToDateTime("2020-01-06 07:00:00"), Convert.ToDateTime("2020-01-06 08:00:00")));
+            eventos.Add(new Evento(4, "Tutorial PagSeguro", "#FFD700", "", Convert.ToDateTime("2020-01-08 15:00:00"), Convert.ToDateTime("2020-01-08 16:00:00")));
+            eventos.Add(new Evento(5, "Tutorial FullCalendar editar detalhes do evento", "#0071c5", "", Convert.ToDateTime("2020-01-08 15:00:00"), Convert.ToDateTime("2020-01-08 16:00:00")));
+            eventos.Add(new Evento(6, "Tutorial FullCalendar Visualizar detalhes do evento", "#42d29d", "", Convert.ToDateTime("2020-01-08 15:00:00"), Convert.ToDateTime("2020-01-08 16:00:00")));
+
+            return Json(eventos);
+        }
+
+
+   
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProjetoCalendario/src/ProjetoWebCalendario/Models/Evento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoWebCalendario.Models
+{
+    public class Evento
+    {
+
+        public Evento() { }
+
+        public int id { get; set; }
+        public string title { get; set; }
+        public string color { get; set; }
+        public string url { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+
+        public Evento(int id, string title, string color, string url, DateTime start, DateTime end)
+        {
+            this.id = id;
+            this.title = title;
+            this.color = color;
+            this.url = url;
+            this.start = start;
+            this.end = end;
+        }
+
+    }
+}
